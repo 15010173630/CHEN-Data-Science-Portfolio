c6d3aef0a59bbfac9e3d5f3f7b9ad5eaf777fdd9
YuJun Chen <15010173630kevin@gmail.com>
2026-09-15 11:02:11 -0400

    basic_streamlit_app Video Game Sales test

---
+++ basic_streamlit_app/main.py
@@ -0,0 +1,4 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.read_csv("basic_streamlit_app/data/vgsales.csv")
--- week_3/my_app.py
+++ week_3/my_app.py
@@ -10,4 +10,14 @@ else:
 import pandas as pd
 st.subheader("Exploring Our Dataset")
 
-df = pd.read_csv("/Users/chenyujun/Documents/GitHub/CHEN-Data-Science-Portfolio/week_3/data/sample_data-1.csv")
+df = pd.read_csv("data/sample_data-1.csv")
+# if in Week_2 (. out week_3)
+# pd.read_csv("./Week_2/sample_data.csv")
+
+st.write("Here's out data")
+st.dataframe(df)
+
+city = st.selectbox('Select a city', df["City"].unique())
+st.write(f"People in {city}")
+st.dataframe(df[df["City"] == city])
+st.bar_chart(df["Salary"])
